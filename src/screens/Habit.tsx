969235901f6fd4a8535e69bb56f823b9e8fe117a
salvatoreDeploy <liderhenrique@gmail.com>
2023-01-27 16:14:42 -0300

    feat Screen Hbait

--- src/screens/Habit.tsx
+++ src/screens/Habit.tsx
@@ -1,9 +1,35 @@
 import React from "react";
+import dayjs from "dayjs";
 
-import { View } from "react-native";
+import { ScrollView, Text, View } from "react-native";
+import { useRoute } from "@react-navigation/native";
+import { BackButton } from "../components/BackButton";
+
+interface Params {
+  date: string;
+}
 
 export function Habit() {
-  return <View className="flex-1 bg-background px-8 pt-16">
-    
-  </View>;
+  const route = useRoute();
+  const { date } = route.params as Params;
+  const parsedDate = dayjs(date);
+  const dayOfWeek = parsedDate.format("dddd");
+  const dayOfMonth = parsedDate.format("DD/MM");
+
+  return (
+    <View className="flex-1 bg-background px-8 pt-16">
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 100 }}
+      >
+        <BackButton />
+        <Text className="mt-6 text-zinc-400 font-semibold text-base lowercase">
+          {dayOfWeek}
+        </Text>
+        <Text className="mt-2 text-white font-extrabold text-3xl lowercase">
+          {dayOfMonth}
+        </Text>
+      </ScrollView>
+    </View>
+  );
 }
